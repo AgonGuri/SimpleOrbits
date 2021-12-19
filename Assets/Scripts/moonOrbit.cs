10d7388e9e4da0c3601bd8e0cb08afe919be3cd2
AgonGuri <uniagonguri@gmail.com>
2021-12-20 00:09:55 +0100

    comments on the use of RotatePointAroundPivot and TODO

--- Assets/Scripts/moonOrbit.cs
+++ Assets/Scripts/moonOrbit.cs
@@ -16,10 +16,12 @@ public class moonOrbit : MonoBehaviour
 
     void Update()
     {
+        // TODO: move not just around the y axis
+
         transform.position =
-    RotatePointAroundPivot(transform.position,
-                           transform.parent.position,
-                           Quaternion.Euler(0, rotationSpeed * Time.deltaTime, 0));
+    RotatePointAroundPivot(transform.position, // point which has to be moved
+                           transform.parent.position, // pivot where to move around
+                           Quaternion.Euler(0, rotationSpeed * Time.deltaTime, 0)); // angle on which to move around
     }
 
     public static Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion angle)
