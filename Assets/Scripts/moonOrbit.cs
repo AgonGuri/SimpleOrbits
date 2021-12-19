03c199754d1348a9bbc8004b83333949f5148882
AgonGuri <uniagonguri@gmail.com>
2021-12-19 19:24:20 +0100

    dev branch comment

--- Assets/Scripts/moonOrbit.cs
+++ Assets/Scripts/moonOrbit.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class moonOrbit : MonoBehaviour
-{
+{    //devbranch
 
     public Transform target;
     public float rotationSpeed = 1f;
