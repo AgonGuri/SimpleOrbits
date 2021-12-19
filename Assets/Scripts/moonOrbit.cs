418f78990ceb6f142313d2e1c5da3d1ca6c52660
AgonGuri <uniagonguri@gmail.com>
2021-12-19 19:11:49 +0100

    erster commit

---
+++ Assets/Scripts/moonOrbit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moonOrbit : MonoBehaviour
+{
+
+    public Transform target;
+    public float rotationSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position =
+    RotatePointAroundPivot(transform.position,
+                           transform.parent.position,
+                           Quaternion.Euler(0, rotationSpeed * Time.deltaTime, 0));
+    }
+
+    public static Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion angle)
+    {
+        return angle * (point - pivot) + pivot;
+    }
+}
